f5cc896a377708ad6bb2f86db2b4254ef2dfd825
daikikuchi <kikuchi.dai@gmail.com>
2017-02-17 12:36:08 +1300

    Add files via upload

--- moviepopulator.py
+++ moviepopulator.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from movie_database_setup import Movie, Base
 
 engine = create_engine('sqlite:///movieinformation.db')
-# Bind the engine to the metadata of the Base class so that the
-# declaratives can be accessed through a DBSession instance
+
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
